c8f9ff12a4f00644e4d1b4ace4b978f31416450d
Thales Alves <thalesalves758@gmail.com>
2022-05-08 01:14:41 -0300

    Importa a barra inferior mobile e a usa

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,13 @@
 import TopBar from './TopBar';
 import Content from './Content';
+import BottomBarMobile from './BottomBarMobile';
 
 export default function App() {
     return (
         <>
             <TopBar />
             <Content />
+            <BottomBarMobile />
         </>
     );
 }
